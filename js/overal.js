b72ae8712321d5e394e45d94b72c0be27ce31280
bugati <m.upelnieks@inbox.lv>
2014-11-16 19:44:55 +0000

    HTML Project work CA 2

---
+++ js/overal.js
@@ -0,0 +1,20 @@
+﻿jQuery(document).ready(function(){
+	
+	var navOffset = jQuery("#fix").offset().top-3;
+	
+	jQuery(window).scroll(function(){
+		var scrollPos = jQuery(window).scrollTop();
+	
+		if(scrollPos >= navOffset){
+			
+			jQuery("#fix").addClass("busfixed");
+		
+		}else{
+			jQuery("#fix").removeClass("busfixed");
+		}
+		
+	});
+
+});
+
+
---
+++ js/validate.js
@@ -0,0 +1,42 @@
+﻿function checkForm(){
+		
+		var validation = 0;
+		var name = document.getElementById('name').value;
+		var email = document.getElementById('email').value;
+		var subject = document.getElementById('subject').value;
+		var message = document.getElementById('message').value;
+		
+		if(name == ''){
+			document.getElementById('invalid_n').innerHTML = 'Please enter your name!';
+			validation +=1;
+		}else{
+			document.getElementById('invalid_n').innerHTML = '';
+		}
+		
+		if(email == ''){
+			document.getElementById('invalid_e').innerHTML = 'Please enter your email!';
+			validation +=1;
+		}else{
+			document.getElementById('invalid_e').innerHTML = '';
+		}
+		
+		if(subject == ''){
+			document.getElementById('invalid_s').innerHTML = 'Please enter subject!';
+			validation +=1;
+		}else{
+			document.getElementById('invalid_s').innerHTML = '';
+		}
+		if(message == ''){
+			document.getElementById('invalid_m').innerHTML = 'Please enter your message!';
+			validation +=1;
+		}else{
+			document.getElementById('invalid_m').innerHTML = '';
+		}
+			
+		
+		if(validation != 0){
+			return false;
+		}else{
+			return true;
+		}
+}
